67c78fe07a10e2d6053112d83bf983cb5bb015fc
igomezg <igomezg@gmail.com>
2023-04-07 15:02:07 +0200

    Lesson3: User Roles and Authorities. Adding roles for secure API.

---
+++ src/main/java/org/ngomezg/security/ApplicationUserRole.java
@@ -0,0 +1,22 @@
+package org.ngomezg.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static org.ngomezg.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(STUDENT_READ, STUDENT_WRITE, COURSE_READ, COURSE_WRITE));
+
+    private final Set <ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+}
